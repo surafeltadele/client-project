42d8d495ccb406dbe5ac04e455a4034dde5afc16
surafeltadele <preastination19@gmail.com>
2025-12-24 16:15:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-const form = document.getElementById('contactForm');
-const status = document.getElementById('status');
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const message = document.getElementById('message').value.trim();
-
-    if(name === "" || email === "" || message === "") {
-        status.textContent = "Please fill in all fields.";
-        status.style.color = "red";
-        return;
-    }
-
-    status.textContent = "Message sent successfully!";
-    status.style.color = "green";
-
-    form.reset();
+// Dark/Light Mode Toggle
+const themeBtn = document.querySelectorAll('#themeToggle');
+themeBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
 });
